86fba8c446339fd0f62d38ea2818654d1fdbb5fd
nicreasquemiguel <miguelva703@gmail.com>
2025-09-30 19:05:48 -0600

    settings

--- terraza/terraza/settings.py
+++ terraza/terraza/settings.py
@@ -300,11 +300,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR = [
+STATIC_URL = '/static/'
+
+# Archivos estáticos de las apps + /static en desarrollo
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Carpeta donde se copiarán todos los estáticos al hacer collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
